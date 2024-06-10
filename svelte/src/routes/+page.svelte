00db841b701ec638d118e2ada4ade6101915658a
kaarolina99 <kapaa008@osloskolen.no>
2024-06-10 19:33:24 +0200

    css greier

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,21 +1,24 @@
 <script>
-import kniv from "./img/bloodyknife.jpeg";
+    import kniv from "./img/bloodyknife.jpeg";
+    import Navbar from "./Navbar.svelte";
 </script>
 
 <div>
-    <div>
+    <Navbar></Navbar>
     <h1 class = "MMCTF"> Murder Mystery CTF </h1>
-    <!-- <img class="imgknife" src={kniv} alt="knife"> -->
         <div>
+            <h4 class="discription1"> I denne spennende CTF utfordringen, inntar du rollen som en detektiv som må løse et grusomt mord.</h4>
+            <h4 class="discription2"> Du må finne liket, identifisere gjerningsvåpenet og samle biologiske spor for å avsløre morderen. </h4>
+            <h4 class="discription3"> Bruk dine ferdigheter innen etterforskning og problemløsning for å hjelpe detektiven med å løse saken og bringe rettferdighet til offeret.</h4>
+            <h4 class="discription4"> Klarer du å samle alle bevisene og løse mysteriet? Din hjelp er avgjørende for å oppklare denne mystiske forbrytelsen. </h4>
             <div>
-
+                <form>
+                    
+                </form>
             </div>
         </div>
-    </div>
     <img class="imgknife" src={kniv} alt="knife">
-</div> 
-
-
+</div>
 
 <style>
 
@@ -35,16 +38,76 @@ import kniv from "./img/bloodyknife.jpeg";
         text-shadow: 2px 1px darkslategrey;
 
     }
+    .discription1{
+        display: flex;
+        align-items: center;
+        color: white;
+        position: absolute;
+        margin-left: 20px;
+        margin-top: 90px;
+        text-shadow: 2px 1px darkslategrey;
+    }
+    .discription2{
+        display: flex;
+        align-items: center;
+        color: white;
+        position: absolute;
+        margin-left: 20px;
+        margin-top: 110px;
+        text-shadow: 2px 1px darkslategrey;
+    }
+    .discription3{
+        display: flex;
+        align-items: center;
+        color: white;
+        position: absolute;
+        margin-left: 20px;
+        margin-top: 130px;
+        text-shadow: 2px 1px darkslategrey;
+    }
+
+    .discription4{
+        display: flex;
+        align-items: center;
+        color: white;
+        position: absolute;
+        margin-left: 20px;
+        margin-top: 150px;
+        text-shadow: 2px 1px darkslategrey;
+    }
 
     :global(body){
         position: relative;
         background-color: black;
-        background-image: url(https://github.com/kaarolina99/Eksamen-CTF/blob/main/svelte/src/routes/img/bloodyknife.jpeg);
-        /* background-size: cover; */
+        background-image: url("https://github.com/kaarolina99/Eksamen-CTF/blob/main/svelte/src/routes/img/bloodyknife.jpeg");
         background-repeat: no-repeat; 
         background-attachment: fixed;
         margin: 0;
-        
-        /* height: 100vh; */
     }
 </style>
+
+
+<!-- <h1>Skriv deg opp i kø</h1>
+                <form id="queueList" on:submit={handleSubmit}>
+                    <label for="Navn"> Navn </label>
+                    <br />
+                    <input
+                        type="text"
+                        id="Navn_"
+                        name="navn"
+                        bind:value={navnInput}
+                        on:keydown={handleKeyDown}
+                    />
+                    <br />
+                    <label for="Sted"> Sted </label>
+                    <br />
+                    <input
+                        type="text"
+                        id="Sted_"
+                        name="sted"
+                        bind:value={stedInput}
+                        on:keydown={handleKeyDown}
+                    />
+                    <br /><br />
+                    <input type="Submit" value="Submit">
+                </form> -->
---
+++ svelte/src/routes/Navbar.svelte
@@ -0,0 +1,5 @@
+<!-- <div class="topnav" id="myTopnav">
+    <a href="/"></a>
+    <a href="/FAQ"></a>
+    <a href="/profile">Profile</a>
+</div> -->
